9fad98289ac9dd9b60379ef233ac5760098217d1
westse <10818305+westse@users.noreply.github.com>
2023-02-07 13:39:54 -0700

    Support complex polymporphic models - Awaits recursive calls to be finished and all sub-models are available before fixing up defined models.

--- springdoc-openapi-common/src/main/java/org/springdoc/core/converters/PolymorphicModelConverter.java
+++ springdoc-openapi-common/src/main/java/org/springdoc/core/converters/PolymorphicModelConverter.java
@@ -25,7 +25,11 @@ package org.springdoc.core.converters;
 import java.lang.reflect.Modifier;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.JavaType;
@@ -33,6 +37,8 @@ import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.core.converter.ModelConverter;
 import io.swagger.v3.core.converter.ModelConverterContext;
 import io.swagger.v3.core.util.AnnotationsUtils;
+import io.swagger.v3.core.util.RefUtils;
+import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.ComposedSchema;
 import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
@@ -48,6 +54,10 @@ public class PolymorphicModelConverter implements ModelConverter {
 	 * The Spring doc object mapper.
 	 */
 	private final ObjectMapperProvider springDocObjectMapper;
+
+	private final AtomicInteger recursionCounter = new AtomicInteger();
+
+	private final Map<String, JavaType> nameToType = new LinkedHashMap<>();
 
 
 	/**
@@ -69,30 +79,105 @@ public class PolymorphicModelConverter implements ModelConverter {
 		return resolvedSchema;
 	}
 
+	private <T> T withCount(Supplier<T> supplier) {
+		recursionCounter.incrementAndGet();
+		try {
+			return supplier.get();
+		}
+		finally {
+			recursionCounter.decrementAndGet();
+		}
+	}
+
 	@Override
 	public Schema resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
 		JavaType javaType = springDocObjectMapper.jsonMapper().constructType(type.getType());
 		if (javaType != null) {
 			if (chain.hasNext()) {
-				Schema<?> resolvedSchema = chain.next().resolve(type, context, chain);
-				resolvedSchema = getResolvedSchema(javaType, resolvedSchema);
-				if (resolvedSchema == null || resolvedSchema.get$ref() == null)
-					return resolvedSchema;
-				return composePolymorphicSchema(type, resolvedSchema, context.getDefinedModels().values());
+				Schema schema = withCount(() -> chain.next().resolve(type, context, chain));
+				if (schema == null) {
+					return schema;
+				}
+				schema = getResolvedSchema(javaType, schema);
+				if (schema.getName() != null) {
+					nameToType.put(schema.getName(), javaType);
+				}
+				if (recursionCounter.get() > 0) {
+					return schema;
+				}
+
+				// This is the original non-recursive schema, so apply polymorphic fixups
+				Collection<Schema> schemas = context.getDefinedModels().values();
+				if (schema.get$ref() != null) {
+					schema = composePolymorphicSchema(schema, schemas);
+				}
+				findAndReplacePolymorphicSchemas(schema, schemas);
+
+				// Also fixup references in previously defined models
+				for (Schema prevDefinedSchema : schemas) {
+					findAndReplacePolymorphicSchemas(prevDefinedSchema, schemas);
+				}
+
+				return schema;
 			}
 		}
 		return null;
 	}
 
+	private void findAndReplacePolymorphicSchemas(Schema schema, Collection<Schema> schemas) {
+		// Schema is not itself a ref but may have properties that are, so convert those.
+		findAndReplacePolymorphicProps(schema, schemas);
+		findAndReplacePolymorphicAllOf(schema, schemas);
+		findAndReplacePolymorphicItems(schema, schemas);
+	}
+
+	private Schema findAndReplacePolymorphicProps(Schema<?> schema, Collection<Schema> schemas) {
+		if (schema.getProperties() != null) {
+			Map<String, Schema> newProps = schema.getProperties().entrySet().stream()
+					.collect(Collectors.toMap(Map.Entry::getKey, entry -> {
+						Schema propSchema = entry.getValue().get$ref() != null
+								? composePolymorphicSchema(entry.getValue(), schemas)
+								: entry.getValue();
+						findAndReplacePolymorphicItems(propSchema, schemas);
+						return propSchema;
+					}));
+			schema.setProperties(newProps);
+		}
+		return schema;
+	}
+
+	private Schema findAndReplacePolymorphicAllOf(Schema<?> schema, Collection<Schema> schemas) {
+		if (schema.getAllOf() != null) {
+			for (Object element : schema.getAllOf()) {
+				if (element instanceof ObjectSchema) {
+					findAndReplacePolymorphicProps((ObjectSchema)element, schemas);
+					// TODO: needed?
+					findAndReplacePolymorphicItems((ObjectSchema)element, schemas);
+				}
+			}
+		}
+		return schema;
+	}
+
+	private Schema findAndReplacePolymorphicItems(Schema<?> schema, Collection<Schema> schemas) {
+		if (schema.getItems() != null && schema.getItems().get$ref() != null && schema instanceof ArraySchema) {
+			schema.setItems(composePolymorphicSchema(schema.getItems(), schemas));
+		}
+		else if (schema.getItems() != null && schema.getItems().get$ref() != null) {
+			System.out.println("@@@ PMC.composePolymorphicItems. items.class=" + schema.getItems().getClass());
+		}
+
+		return schema;
+	}
+
 	/**
 	 * Compose polymorphic schema schema.
 	 *
-	 * @param type the type
 	 * @param schema the schema
 	 * @param schemas the schemas
 	 * @return the schema
 	 */
-	private Schema composePolymorphicSchema(AnnotatedType type, Schema schema, Collection<Schema> schemas) {
+	private Schema composePolymorphicSchema(Schema schema, Collection<Schema> schemas) {
 		String ref = schema.get$ref();
 		List<Schema> composedSchemas = schemas.stream()
 				.filter(ComposedSchema.class::isInstance)
@@ -104,7 +189,7 @@ public class PolymorphicModelConverter implements ModelConverter {
 		if (composedSchemas.isEmpty()) return schema;
 
 		ComposedSchema result = new ComposedSchema();
-		if (isConcreteClass(type)) result.addOneOfItem(schema);
+		if (isConcreteClass(ref)) result.addOneOfItem(schema);
 		composedSchemas.forEach(result::addOneOfItem);
 		return result;
 	}
@@ -112,11 +197,15 @@ public class PolymorphicModelConverter implements ModelConverter {
 	/**
 	 * Is concrete class boolean.
 	 *
-	 * @param type the type
+	 * @param ref the ref
 	 * @return the boolean
 	 */
-	private boolean isConcreteClass(AnnotatedType type) {
-		JavaType javaType = springDocObjectMapper.jsonMapper().constructType(type.getType());
+	private boolean isConcreteClass(String ref) {
+		String name = (String) RefUtils.extractSimpleName(ref).getLeft();
+		JavaType javaType = nameToType.get(name);
+		if (javaType == null) {
+			return false;
+		}
 		Class<?> clazz = javaType.getRawClass();
 		return !Modifier.isAbstract(clazz.getModifiers()) && !clazz.isInterface();
 	}
